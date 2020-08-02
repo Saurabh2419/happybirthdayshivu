9ce5f0de8e25d73ef69126be9339da2cc02818f1
Saurabh2419 <67796158+Saurabh2419@users.noreply.github.com>
2020-08-02 10:37:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "Shivani"&&b=="2403")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
